903998804bfcf94723b68496e25336c0a4b4bfb9
Vilius Paulauskas <viliusp@gmail.com>
2017-12-11 20:17:35 +0100

    Configure used modules by Fluxus.

--- scripts/build-config.js
+++ scripts/build-config.js
@@ -1,18 +1,9 @@
 module.exports = {
   components: [
-    'virtual',
     'keyboard',
-    'mousewheel',
     'navigation',
     'pagination',
-    'scrollbar',
-    'parallax',
-    'zoom',
     'lazy',
-    'controller',
-    'a11y',
-    'history',
-    'hash-navigation',
     'autoplay',
     'effect-fade',
     'effect-cube',
